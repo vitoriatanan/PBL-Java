332cc9832828e1df838ebd467522d9b61dfdeeb0
vitoriatanan <vitoriatanans@gmail.com>
2023-11-20 14:44:05 -0300

    📚 Implementação da classe Bibliotecario

--- src/main/java/Bibliotecario/Bibliotecario.java
+++ src/main/java/Bibliotecario/Bibliotecario.java
@@ -3,7 +3,7 @@ package Bibliotecario;
 import Pessoa.Pessoa;
 
 public class Bibliotecario extends Pessoa {
-    // Atributos K
+    // Atributos
     private String cargo;
     private String senhaAcesso;
 
