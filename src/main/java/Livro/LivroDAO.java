8c99e730995c9b158cebc87f4a68835618070e17
vitoriatanan <vitoriatanans@gmail.com>
2023-11-20 14:54:57 -0300

    📚 Implementação da classe Livro

--- src/main/java/Livro/LivroDAO.java
+++ src/main/java/Livro/LivroDAO.java
@@ -1,9 +1,8 @@
 package Livro;
 
 import java.util.List;
-//
 public interface LivroDAO {
-    List<Livro> create(Livro livro); //adiciona um Livro
+    List<Livro> create(Livro livro);
     Livro read(String titulo);
     void update(String titulo, Livro novoLivro);
     List<Livro> delete(Livro livro);
