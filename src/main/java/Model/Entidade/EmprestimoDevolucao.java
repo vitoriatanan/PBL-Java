fea5f35c879b30d5afc15b3b97d49de098167762
vitoriatanan <vitoriatanans@gmail.com>
2023-12-02 22:18:01 -0300

    📚 Implementação da classe EmprestimoDevolucao

---
+++ src/main/java/Model/Entidade/EmprestimoDevolucao.java
@@ -0,0 +1,64 @@
+package Model.Entidade;
+
+import java.util.Date;
+
+public class EmprestimoDevolucao {
+    // Atributos
+    private Livro livro;
+    private Usuario usuario;
+    private Date dataEmprestimo;
+    private Date dataDevolucaoEsperada;
+    private Date dataDevolucaoReal;
+    private int renovacoes;
+
+    // Método Construtor
+    public EmprestimoDevolucao(Usuario usuario, Livro livro, Date dataEmprestimo, Date dataDevolucaoEsperada) {
+        this.livro = livro;
+        this.usuario = usuario;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucaoEsperada = dataDevolucaoEsperada;
+        this.renovacoes = 0;
+    }
+
+
+    public Date getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(Date dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public Date getDataDevolucaoEsperada() {
+        return dataDevolucaoEsperada;
+    }
+
+    public void setDataDevolucaoEsperada(Date dataDevolucaoEsperada) {
+        this.dataDevolucaoEsperada = dataDevolucaoEsperada;
+    }
+
+    public Date getDataDevolucaoReal() {
+        return dataDevolucaoReal;
+    }
+
+    public void setDataDevolucaoReal(Date dataDevolucaoReal) {
+        this.dataDevolucaoReal = dataDevolucaoReal;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public int getRenovacoes() {
+        return renovacoes;
+    }
+
+    public void contagemRenovacoes() {
+        renovacoes++;
+    }
+
+}
