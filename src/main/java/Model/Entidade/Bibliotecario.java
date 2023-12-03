ba1b1b7d4213b04bb9096f99fcc6ec8a4927895d
vitoriatanan <vitoriatanans@gmail.com>
2023-12-02 22:17:37 -0300

    📚 Implementação da classe Bibliotecario

---
+++ src/main/java/Model/Entidade/Bibliotecario.java
@@ -0,0 +1,32 @@
+package Model.Entidade;
+
+public class Bibliotecario extends Pessoa {
+    // Atributos
+    private String cargo;
+    private String senhaAcesso;
+
+    // Método Construtor
+    public Bibliotecario(String nome, Integer id, String cargo, String senhaAcesso) {
+        super(nome, id);
+        this.cargo = cargo;
+        this.senhaAcesso = senhaAcesso;
+    }
+
+    // Métodos Acessores
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSenhaAcesso() {
+        return senhaAcesso;
+    }
+
+    public void setSenhaAcesso(String senhaAcesso) {
+        this.senhaAcesso = senhaAcesso;
+    }
+
+}
