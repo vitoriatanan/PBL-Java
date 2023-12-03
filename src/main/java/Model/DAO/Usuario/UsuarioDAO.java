af2893d280e793a8864b9cf9e87699753bd52d51
vitoriatanan <vitoriatanans@gmail.com>
2023-12-02 22:14:28 -0300

    📚 Implementação da interface DAO do Usuario

---
+++ src/main/java/Model/DAO/Usuario/UsuarioDAO.java
@@ -0,0 +1,13 @@
+package Model.DAO.Usuario;
+
+import Model.Entidade.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDAO {
+    List<Usuario> create(Usuario usuario);
+    Usuario read(Integer id);
+    void update(Integer id, Usuario novoUsuario);
+    List<Usuario> delete(Usuario usuario);
+}
+
