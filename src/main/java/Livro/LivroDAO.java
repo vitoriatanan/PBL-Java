363647b6af565a94242e70c32900a0523246c7b7
vitoriatanan <vitoriatanans@gmail.com>
2023-12-02 22:21:48 -0300

    Delete LivroDAO.java

--- src/main/java/Livro/LivroDAO.java
+++
@@ -1,9 +0,0 @@
-package Livro;
-
-import java.util.List;
-public interface LivroDAO {
-    List<Livro> create(Livro livro);
-    Livro read(String titulo);
-    void update(String titulo, Livro novoLivro);
-    List<Livro> delete(Livro livro);
-}
