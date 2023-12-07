83e1f6854c85e7698f5bd4774d030d3e445f03e1
vitoriatanan <vitoriatanans@gmail.com>
2023-12-07 09:55:03 -0300

    📚 Implementação da interface DAO do Administrador

--- src/main/java/Model/DAO/Administrador/AdministradorDAO.java
+++ src/main/java/Model/DAO/Administrador/AdministradorDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface AdministradorDAO {
     List<Administrador> create(Administrador administrador);
-    Administrador read(Integer id);
-    void update(Integer id, Administrador novoAdm);
+    Administrador read(String id);
+    void update(String id, Administrador novoAdm);
     List<Administrador> delete(Administrador administrador);
 }
