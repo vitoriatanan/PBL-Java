4981e8f66f60b2def5c522afe787f35e07998489
vitoriatanan <vitoriatanans@gmail.com>
2023-12-12 19:55:31 -0300

    📚 Implementação da interface DAO do Usuário

--- src/main/java/Model/DAO/Usuario/UsuarioDAO.java
+++ src/main/java/Model/DAO/Usuario/UsuarioDAO.java
@@ -2,12 +2,14 @@ package Model.DAO.Usuario;
 
 import Model.Entidade.Usuario;
 
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
 import java.util.List;
 
 public interface UsuarioDAO {
-    List<Usuario> create(Usuario usuario);
-    Usuario read(String id);
-    void update(String id, Usuario novoUsuario);
-    List<Usuario> delete(Usuario usuario);
+    List<Usuario> create(Usuario usuario) throws IOException;
+    Usuario read(String id) throws IOException, ClassNotFoundException;
+    void update(String id, Usuario novoUsuario) throws IOException, ClassNotFoundException;
+    List<Usuario> delete(Usuario usuario) throws IOException, ClassNotFoundException;
 }
 
