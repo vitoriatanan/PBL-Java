2e91ced7139a2c81941f5cb63c80475e3029189e
vitoriatanan <vitoriatanans@gmail.com>
2023-12-12 19:52:43 -0300

    📚 Implementação da interface DAO de EmprestismoDevolucao

--- src/main/java/Model/DAO/EmprestimoDevolucao/EmprestimoDevolucaoDAO.java
+++ src/main/java/Model/DAO/EmprestimoDevolucao/EmprestimoDevolucaoDAO.java
@@ -2,11 +2,12 @@ package Model.DAO.EmprestimoDevolucao;
 
 import Model.Entidade.EmprestimoDevolucao;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface EmprestimoDevolucaoDAO {
-    List<EmprestimoDevolucao> create(EmprestimoDevolucao emprestimo);
-    EmprestimoDevolucao read(EmprestimoDevolucao emprestimo);
-    void update(EmprestimoDevolucao emprestimoAtual, EmprestimoDevolucao novoEmprestimo);
-    void delete(EmprestimoDevolucao emprestimo);
+    List<EmprestimoDevolucao> create(EmprestimoDevolucao emprestimo) throws IOException;
+    EmprestimoDevolucao read(String titulo) throws IOException, ClassNotFoundException;
+    void update(EmprestimoDevolucao emprestimoAtual, EmprestimoDevolucao novoEmprestimo) throws IOException, ClassNotFoundException;
+    void delete(EmprestimoDevolucao emprestimo) throws IOException, ClassNotFoundException;
 }
