ad32fc2c2d3c145bb036e8e2c4902a0bc347d23d
vitoriatanan <vitoriatanans@gmail.com>
2023-12-12 19:53:46 -0300

    📚 Implementação DAO do Livro

--- src/main/java/Model/DAO/Livro/LivroDAOImpl.java
+++ src/main/java/Model/DAO/Livro/LivroDAOImpl.java
@@ -1,8 +1,10 @@
 package Model.DAO.Livro;
 
-import Model.DAO.Livro.LivroDAO;
+import FileData.Entidade.Serializador;
+import Model.Entidade.Bibliotecario;
 import Model.Entidade.Livro;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -14,15 +16,40 @@ public class LivroDAOImpl implements LivroDAO {
         this.livros = new ArrayList<>();
     }
 
+    /**
+     * Responsável por adicionar um novo livro ao sistema.
+     *
+     * Adiciona o livro à lista de livross e salva a lista atualizada
+     * de livros no arquivo "livro.dat".
+     *
+     * @param livro     livro a ser adicionado
+     * @return          lista de livros
+     * */
     @Override
-    public List<Livro> create(Livro livro) {
+    public List<Livro> create(Livro livro) throws IOException {
         livros.add(livro);
+        Serializador.salvarDados("livro.dat", livros);
         return livros;
     }
 
+    /**
+     * Responsável por procurar um livro com um id específico
+     * na lista de livros.
+     *
+     * Faz a leitura do arquivo "livro.dat", itera sobre a lista
+     * e verifica se cada livro possui o titulo.
+     *
+     * @param titulo    titulo do usuario a ser procurado
+     * @return          livro que possui o titulo ou nulo se o livro não foi encontrado
+     * */
     @Override
-    public Livro read(String titulo) {
-        for (Livro livro : livros) {
+    public Livro read(String titulo) throws IOException, ClassNotFoundException {
+
+        /* Faz a leitura do arquivo "livro.dat" */
+        ArrayList<Livro> listaLivro = new ArrayList<Livro>();
+        listaLivro = (ArrayList<Livro>) Serializador.leituraDados("livro.dat");
+
+        for (Livro livro : listaLivro) {
             if (livro.getTitulo().equalsIgnoreCase(titulo)) {
                 return livro;
             }
@@ -30,19 +57,65 @@ public class LivroDAOImpl implements LivroDAO {
         return null;
     }
 
+
+    /**
+     * Responsável por atualizar informações de um livro no sistema.
+     *
+     * Faz a leitura do arquivo para obter a lista atual de livros,
+     * remove o livro antigo, adiciona o novo livro à lista e salva
+     * no arquivo "livro.dat".
+     *
+     * @param titulo          título do livro
+     * @param novoLivro       dados do livro a serem atualizados
+     * */
     @Override
-    public void update(String titulo, Livro novoLivro) {
-        Livro livroAntigo = read(titulo);
-        if (livroAntigo != null) {
-            livros.remove(livroAntigo);
-            livros.add(novoLivro);
+    public void update(String titulo, Livro novoLivro) throws IOException, ClassNotFoundException {
+        ArrayList<Livro> listaLivro = (ArrayList<Livro>) Serializador.leituraDados("livro.dat");
+
+        for (int i = 0; i < listaLivro.size(); i++) {
+            if (listaLivro.get(i).getTitulo().equals(titulo)) {
+                // Atualiza apenas os campos necessários
+                listaLivro.get(i).setAutor(novoLivro.getAutor());
+                listaLivro.get(i).setEditora(novoLivro.getEditora());
+                listaLivro.get(i).setIsbn(novoLivro.getIsbn());
+                listaLivro.get(i).setAnoPublicacao(novoLivro.getAnoPublicacao());
+                listaLivro.get(i).setCategoria(novoLivro.getCategoria());
+
+                Serializador.salvarDados("livro.dat", listaLivro);
+                break;
+            }
         }
     }
 
+
+    /**
+     * Responsável por remover um livro do sistema.
+     *
+     * Faz a leitura do arquivo para obter a lista atual de livros,
+     * cria uma cópia dessa lista para iterar sobre a copia, verifica
+     * o livro a ser removido, remove o livro e salva a lista
+     * modificada no arquivo "livro.dat".
+     *
+     * @param livro     livro a ser removido.
+     * @return          lista atualizada.
+     * */
     @Override
-    public List<Livro> delete(Livro livro) {
-        this.livros.remove(livro);
-        return livros;
+    public List<Livro> delete(Livro livro) throws IOException, ClassNotFoundException {
+
+        /* Faz a leitura do arquivo "livro.dat" */
+        ArrayList<Livro> listaLivro = new ArrayList<Livro>();
+        listaLivro = (ArrayList<Livro>) Serializador.leituraDados("livro.dat");
+
+        /* Cria uma cópia da lista de listaLivros */
+        List<Livro> copiaLista = new ArrayList<>(listaLivro);
+
+        for (Livro liv : copiaLista) {
+            if (liv.equals(livro)) {
+                listaLivro.remove(livro);
+            }
+        }
+        Serializador.salvarDados("livro.dat", listaLivro);
+        return listaLivro;
     }
 
     public boolean pesquisarLivro(String tipo, String busca) {
@@ -59,7 +132,6 @@ public class LivroDAOImpl implements LivroDAO {
                 return true;
             }
         }
-
         return false;
     }
 
