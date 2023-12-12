8fc208d9427adce199da6027ed04dbcc9c5321e9
vitoriatanan <vitoriatanans@gmail.com>
2023-12-12 19:56:22 -0300

    📚 Implementação da classe Administrador

--- src/main/java/Model/Entidade/Administrador.java
+++ src/main/java/Model/Entidade/Administrador.java
@@ -2,14 +2,23 @@ package Model.Entidade;
 
 public class Administrador extends Pessoa {
     // Atributos
+    private String id;
     private String cargo;
     private String senhaAcesso;
 
     // Método Construtor
-    public Administrador(String nome, String id,  String cargo, String senhaAcesso) {
-        super(nome, id);
+    public Administrador(String nome, String cargo, String senhaAcesso) {
+        super(nome);
         this.cargo = cargo;
         this.senhaAcesso = senhaAcesso;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getCargo() {
