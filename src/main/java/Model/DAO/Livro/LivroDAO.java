504bb966eac9827e0e1327a8eb110d726f9a0200
vitoriatanan <vitoriatanans@gmail.com>
2023-12-12 19:53:20 -0300

    📚 Implementação da interface DAO do Livro

--- src/main/java/Model/DAO/Livro/LivroDAO.java
+++ src/main/java/Model/DAO/Livro/LivroDAO.java
@@ -2,10 +2,11 @@ package Model.DAO.Livro;
 
 import Model.Entidade.Livro;
 
+import java.io.IOException;
 import java.util.List;
 public interface LivroDAO {
-    List<Livro> create(Livro livro);
-    Livro read(String titulo);
-    void update(String titulo, Livro novoLivro);
-    List<Livro> delete(Livro livro);
+    List<Livro> create(Livro livro) throws IOException;
+    Livro read(String titulo) throws IOException, ClassNotFoundException;
+    void update(String titulo, Livro novoLivro) throws IOException, ClassNotFoundException;
+    List<Livro> delete(Livro livro) throws IOException, ClassNotFoundException;
 }
