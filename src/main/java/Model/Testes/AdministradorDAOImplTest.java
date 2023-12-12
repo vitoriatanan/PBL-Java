aea3603c220a738c59448505b54a20b0b4ae06c9
vitoriatanan <vitoriatanans@gmail.com>
2023-12-12 20:10:41 -0300

    ✨ Implementação de testes da classe AdministradorDAOImpl

--- src/main/java/Model/Testes/AdministradorDAOImplTest.java
+++ src/main/java/Model/Testes/AdministradorDAOImplTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AdministradorDAOImplTest {
     private AdministradorDAOImpl administradorDAO;
     private Administrador admTeste, admTeste2;
+    
     @BeforeEach
     public void setUp() {
         administradorDAO = new AdministradorDAOImpl();
