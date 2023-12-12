b1fafdd7c1c4e95b7e3d8699369d4342efa165ec
vitoriatanan <vitoriatanans@gmail.com>
2023-12-12 19:51:46 -0300

    📚 Implementação da interface DAO do Bibliotecário

--- src/main/java/Model/DAO/Bibliotecario/BibliotecarioDAO.java
+++ src/main/java/Model/DAO/Bibliotecario/BibliotecarioDAO.java
@@ -2,11 +2,12 @@ package Model.DAO.Bibliotecario;
 
 import Model.Entidade.Bibliotecario;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface BibliotecarioDAO {
-    List<Bibliotecario> create(Bibliotecario bibliotecario);
-    Bibliotecario read(String id);
-    void update(String id, Bibliotecario novoBibliotecario);
-    List<Bibliotecario> delete(Bibliotecario bibliotecario);
+    List<Bibliotecario> create(Bibliotecario bibliotecario) throws IOException;
+    Bibliotecario read(String id) throws IOException, ClassNotFoundException;
+    void update(String id, Bibliotecario novoBibliotecario) throws IOException, ClassNotFoundException;
+    List<Bibliotecario> delete(Bibliotecario bibliotecario) throws IOException, ClassNotFoundException;
 }
