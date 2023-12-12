9eaeb1bbb80ae987106c73dbfa57d48f26a21fd7
vitoriatanan <vitoriatanans@gmail.com>
2023-12-12 19:50:18 -0300

    📚 Implementação da interface DAO do Administrador

--- src/main/java/Model/DAO/Administrador/AdministradorDAO.java
+++ src/main/java/Model/DAO/Administrador/AdministradorDAO.java
@@ -2,11 +2,12 @@ package Model.DAO.Administrador;
 
 import Model.Entidade.Administrador;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface AdministradorDAO {
-    List<Administrador> create(Administrador administrador);
-    Administrador read(String id);
-    void update(String id, Administrador novoAdm);
-    List<Administrador> delete(Administrador administrador);
+    List<Administrador> create(Administrador administrador) throws IOException;
+    Administrador read(String id) throws IOException, ClassNotFoundException;
+    void update(String id, Administrador novoAdm) throws IOException, ClassNotFoundException;
+    List<Administrador> delete(Administrador administrador) throws IOException, ClassNotFoundException;
 }
